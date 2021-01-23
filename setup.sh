fa5bd0eef0bdbb2e7f44db1bf86fc409863fc694
Gharbaoui <mohamed9gharbaoui@gmail.com>
2021-01-23 12:27:37 +0100

    working ftps without teleghraf

--- setup.sh
+++ setup.sh
@@ -1,33 +1,39 @@
-# start of functions 
-function print_line(){ # first param is name of the service second param is color	
+function print_line(){
 	color=$2
-	svc=$1
-	echo "${color}================== ${svc} ====================${NORM}"
-	echo "";
+	echo "${color}=========================== '  $1 ' =====================================${NORM}";
+	echo "" ;
 }
 
-function replace_ip()
+function replace_ip () # what to replace $2 with what $3 file path
 {
 	sed -i '' "s/$1/$2/g" $3;
 }
 
-#end of functions
 
-# start of env variables
-192.168.99.159export YELLOW='\033[1;33m'
+# VARIABLES
+export YELLOW='\033[1;33m'
 export GREEN='\033[0;32m'
 export NORM='\033[0m'
-export MKIP=$(minikube ip)
+
+# starting MINIKUBE
+export MINIKUBE_HOME=~/goinfre/
+minikube start --driver=virtualbox
 eval $(minikube docker-env)
-#end of  env variables
+export MKIP=$(minikube ip)
+# MINIKUBE STARTED
 
+# installing metallb
+print_line 'instaling metallb' $YELLOW
+replace_ip 'MINIKUBE_IP' $MKIP './req/config.yaml'
+sh ./req/install.sh
+replace_ip $MKIP 'MINIKUBE_IP' './req/config.yaml'
+print_line 'metallb instaled' $GREEN
+# metallb installed
 
-
-# installing ftps
-print_line 'INSTALLING FTPS server' $YELLOW
+# installing ftps username is 'mohamed' password '1234'
+print_line 'installing FTPS server' $YELLOW
 replace_ip 'MINIKUBE_IP' $MKIP './src/FTPS/image/vsftpd.conf'
 sh ./src/FTPS/start.sh
-print_line 'FTPS server installed' $GREEN
 replace_ip $MKIP 'MINIKUBE_IP' './src/FTPS/image/vsftpd.conf'
+print_line 'FTPS server installed' $GREEN
 # ftps installed
-
